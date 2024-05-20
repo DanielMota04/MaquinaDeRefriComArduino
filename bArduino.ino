8b604713a74324e7551636007b5301447187508b
DanielMota04 <146385923+DanielMota04@users.noreply.github.com>
2024-05-20 15:26:57 -0300

    Alterações feitas

--- bArduino.ino
+++ bArduino.ino
@@ -1,7 +1,7 @@
-const int buttonPin1 = ; // botao do primeiro refri
-const int buttonPin2 = ; // botao do segundo refri
-const int rele1Pin = ; // rele que ativa o primeiro motor
-const int rele2Pin = ; // rele que ativa o segundo motor
+const int buttonPin1 = 1; // botao do primeiro refri
+const int buttonPin2 = 2; // botao do segundo refri
+const int rele1Pin = 7; // rele que ativa o primeiro motor
+const int rele2Pin = 8; // rele que ativa o segundo motor
 int button1State = 0; 
 int button2State = 0;
 
@@ -21,15 +21,18 @@ void loop()
 
   if(button1State == HIGH) // verifica se o botao 1 foi apertado
   {
-    digitalWrite(rele1Pin, HIGH); // liga o 1 relé
-    delay(3000); // conta 3 segundos
-    digitalWrite(rele1Pin, LOW); // desliga o 1 relé
+    ligaDesliga(rele1Pin);
   }
 
   if(button2State == HIGH) // verifica se o botão 2 foi apertado
   {
-    digitalWrite(rele2Pin, HIGH); // liga o 2 relé
-    delay(3000); // conta 3 segundos
-    digitalWrite(rele2Pin, LOW); // desliga o 2 relé
+    ligaDesliga(rele2Pin);
   }
+}
+
+void ligaDesliga(int relePin)
+{
+    digitalWrite(relePin, HIGH); // liga o 1 relé
+    delay(3000); // conta 3 segundos
+    digitalWrite(relePin, LOW); // desliga o 1 relé
 }
