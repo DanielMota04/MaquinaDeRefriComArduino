58cada327444da1e511f3a5a5ea4ef5357451edf
DanielMota04 <146385923+DanielMota04@users.noreply.github.com>
2024-06-03 12:42:31 -0300

    Novas funções adicionadas
    adicionada a possibilidade de rodar a máquina com um timer, porém, uma bomba de cada vez e também a possibilidade de rodar a bomba ao segurar o botão, possibilitando o uso das duas bombas independente e simultaneamente

--- bArduino.ino
+++ bArduino.ino
@@ -1,3 +1,4 @@
+// declaração das variáveis
 const int buttonPin1 = 1; // botao do primeiro refri
 const int buttonPin2 = 2; // botao do segundo refri
 const int rele1Pin = 7; // rele que ativa o primeiro motor
@@ -5,19 +6,20 @@ const int rele2Pin = 8; // rele que ativa o segundo motor
 int button1State = 0; 
 int button2State = 0;
 
-
+// definindo os pinos de saída e entrada
 void setup()
 {
  pinMode(rele1Pin, OUTPUT); // define o primeiro relé como saída
  pinMode(rele2Pin, OUTPUT); // define o segundo relé como saída 
  pinMode(buttonPin1, INPUT); // define o primeiro botao como entrada
  pinMode(buttonPin2, INPUT); // define o segundo botao como entrada
- digitalWrite(rele1Pin, HIGH); // desliga o 1 relé
- digitalWrite(rele2Pin, HIGH); // desliga o 1 relé
+ digitalWrite(rele1Pin, HIGH); // inicia o primeiro relé como desligado
+ digitalWrite(rele2Pin, HIGH); // inicia o segundo relé como desligado
 
  
 }
 
+// funcionamento do código
 void loop()
 {
   button1State = digitalRead(buttonPin1); // define o estado do botao 1 como ligado ou desligado
@@ -26,17 +28,28 @@ void loop()
   if(button1State == LOW) // verifica se o botao 1 foi apertado
   {
     ligaDesliga(rele1Pin);
+    // ligaDesligaComTimer(rele1pin);
   }
 
   if(button2State == LOW) // verifica se o botão 2 foi apertado
   {
     ligaDesliga(rele2Pin);
+    // ligaDesligaComTimer(rele2pin);
   }
+}
+
+// funcao para ligar e desligar o relé
+void ligaDesligaComTimer(int relePin)
+{
+    // Desvantagem: funciona apenas uma bomba por vez
+    digitalWrite(relePin, LOW); // liga o relé
+    delay(3000); // conta 3 segundos
+    digitalWrite(relePin, HIGH); // desliga o relé
 }
 
 void ligaDesliga(int relePin)
 {
-    digitalWrite(relePin, LOW); // liga o 1 relé
-    delay(3000); // conta 3 segundos
-    digitalWrite(relePin, HIGH); // desliga o 1 relé
+    // Pode rodar as duas bombas simultaneamente
+    digitalWrite(relePin, LOW); // liga o relé
+    digitalWrite(relePin, HIGH); // desliga o relé
 }
